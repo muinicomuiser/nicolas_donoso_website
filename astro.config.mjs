124a713fa29dc7daca987e50ebbd3356a3c126f9
muinicomuiser <nicodoneg@gmail.com>
2026-09-07 21:26:44 -0300

    home

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://muinicomuiser.github.io', 
+  base: '/nicolas_donoso_website', 
+});
